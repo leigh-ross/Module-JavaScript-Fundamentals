691b653ab6f3617281cd2eb94f671a6cc7e2dfd1
Tyler-leigh Ross <rossleighit@gmail.com>
2026-09-22 09:34:19 +0200

    Calling functions exercise with comments

---
+++ prep-sprint-3/functions.js
@@ -0,0 +1,22 @@
+console.log(Math.round);
+
+// [Function: round]: tells us that Math.round is a fucntion
+
+console.log(Math.round(10.3));
+
+// 10 : Math.round() rounds to nearest whole number
+
+let result = Math.round(10.3);
+console.log(result);
+
+// Practicing printing to term
+
+let result2 = Math.round(4.2);
+console.log(result2);
+
+// store 2nd value
+
+let frankocean = result + result2;
+console.log(frankocean);
+
+// added var frankocean and stored sum of 2 results. print to term.
